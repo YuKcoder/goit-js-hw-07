bf3adc7ba193999cb85f30c03c9960c1fe8cc8e8
Yurii Kovtunenko <kovtunenko.91.09@gmail.com>
2021-07-20 19:50:03 +0300

    Adds solution of task-4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const counterValue = {
+    value: 0,
+    decrement() {
+        this.value -= 1;
+    },
+    increment() {
+        this.value += 1;
+    },
+};
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', () => {
+    counterValue.decrement();
+    valueEl.textContent = counterValue.value;
+});
+
+incrementBtn.addEventListener('click', () => {
+    counterValue.increment();
+    valueEl.textContent = counterValue.value;
+});
