cdbbb61a4107813039fb50d1378772b00e560f0c
Yurii Kovtunenko <kovtunenko.91.09@gmail.com>
2021-07-20 21:13:30 +0300

    Adds solution of task-5

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -4,7 +4,9 @@ const totalCategories = document.querySelectorAll('li.item');
 console.log(`В списке ${totalCategories.length} категории`);
 
 const arrayCategories = [...totalCategories]
-    .map(categories => `Категория: ${categories.children[0].textContent} (количество элементов: ${categories.children[1].children.length})`)
+    .map(
+        categories =>
+            `Категория: ${categories.children[0].textContent} (количество элементов: ${categories.children[1].children.length})`,
+    )
     .join('\n');
 console.log(arrayCategories);
-
--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const input = document.querySelector('#name-input');
+const nameLabel = document.querySelector('#name-output');
+
+const onInputChange = event =>
+    event.currentTarget.value === ''
+        ? (nameLabel.textContent = 'незнакомец')
+        : (nameLabel.textContent = event.currentTarget.value);
+
+input.addEventListener('input', onInputChange);
