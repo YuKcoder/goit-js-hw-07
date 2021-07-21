4e2c27b72aed3e3a92f3a46d6e1435871955f00c
Yurii Kovtunenko <kovtunenko.91.09@gmail.com>
2021-07-21 23:02:15 +0300

    Adds solution of task-6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,1 +1,20 @@
-'use strict';
+'use strict'
+
+const inputName = document.querySelector('#validation-input');
+
+const onInputBlur = event => {
+    if (event.target.value.length === Number(inputName.dataset.length)) {
+        inputName.classList.add('valid');
+        inputName.classList.remove('invalid');
+    }
+    if (event.target.value.length !== Number(inputName.dataset.length)) {
+        inputName.classList.add('invalid');
+        inputName.classList.remove('valid');
+    }
+    if (event.target.value.length === 0) {
+        inputName.classList.add('#validation-input');
+        inputName.classList.remove('invalid');
+    }
+}
+
+inputName.addEventListener('blur', onInputBlur);
